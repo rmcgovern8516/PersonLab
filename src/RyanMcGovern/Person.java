435cce5413f285916fbe451cbee7f3ae58860af3
BT_1N3_31 <baishakicodes>
2018-10-19 08:42:10 -0400

    abstract method created, not complete.

--- src/RyanMcGovern/Person.java
+++ src/RyanMcGovern/Person.java
@@ -1,4 +1,16 @@
 package RyanMcGovern;
 
 public abstract class Person {
+    private String firstName;
+    private String familyName;
+    public Person(String firstName, String familyName){
+        this.firstName = firstName;
+        this.familyName = familyName;
+    }
+    public abstract String getFirstName();
+    public abstract String getFamilyName();
+    public void String toString(){
+
+    }
+
 }
