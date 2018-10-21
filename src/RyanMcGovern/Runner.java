1045516c2037f0e10d155e18e8b6e3d7525f2c8f
baishakicodes <43207472+baishakicodes@users.noreply.github.com>
2018-10-21 12:57:39 -0400

    Completed the person abstract class as well as the student class but not sure if it meets the requirements of the lab. Tested out both classes in the runner class. Runner class definitely does not have everything required by the lab.

--- src/RyanMcGovern/Runner.java
+++ src/RyanMcGovern/Runner.java
@@ -1,8 +1,20 @@
 package RyanMcGovern;
 
+import jdk.internal.dynalink.beans.StaticClass;
+
 public class Runner {
 
     public static void main(String[] args) {
         // write your code here
+        Student student1 = new Student("Bob", "Bob", 3.5);
+        Student student2 = new Student("Sam", "Robertson", 2.7);
+        Student[] students = {student1, student2};
+        for(Student p : students ){
+            System.out.println(p);
+            System.out.println("My first name is "+p.getFirstName());
+            System.out.println("My family name is "+p.getFamilyName());
+            System.out.println("My GPA name is "+p.getGPA());
+            System.out.println("Is my family name the same as my first name? "+p.equals());
+        }
     }
 }
--- src/RyanMcGovern/Student.java
+++ src/RyanMcGovern/Student.java
@@ -1,4 +1,37 @@
 package RyanMcGovern;
 
-public class Student {
+import java.security.PublicKey;
+
+public class Student extends Person{
+    private String firstName;
+    private String familyName;
+    private double GPA;
+    public Student (String firstName, String familyName, double GPA){
+        super("Student");
+        this.firstName = firstName;
+        this.familyName = familyName;
+        this.GPA = GPA;
+    }
+    @Override
+    public String getFirstName(){
+        return firstName;
+    }
+    @Override
+    public String getFamilyName(){
+        return familyName;
+    }
+    @Override
+    public String toString(){
+        return familyName +","+firstName;
+    }
+    @Override
+    public boolean equals(){
+        if(getFamilyName().equals(getFirstName())){
+            return true;
+        }
+        return false;
+    }
+    public double getGPA(){
+        return GPA;
+    }
 }
