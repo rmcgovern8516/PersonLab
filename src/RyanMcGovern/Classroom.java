366c1e0b8d4208737a3e5edf5e948654dee66488
BT_1N3_30 <cpolisi6526@bths.edu>
2018-10-22 08:41:07 -0400

    1

--- src/RyanMcGovern/Classroom.java
+++ src/RyanMcGovern/Classroom.java
@@ -1,4 +1,16 @@
 package RyanMcGovern;
-
+import RyanMcGovern.Student;
 public class Classroom {
+    private String[] Students;
+        Student s1 = ("Abby", "Aox", 2.56);
+        Student s2 = ("Ben", "Box", 3.2);
+        Student s3 = ("Carly", "Cox", 3.4);
+        Student s4 = ("Damien", "Dox", 1.98);
+        Student s5 = ("Ed", "Eox", 3.7);
+
+        public void listmaker{
+            this.Students= new String[5];
+            Student.getFirstName(s1)
+    }
+
 }
